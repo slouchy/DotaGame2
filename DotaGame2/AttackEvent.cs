c7e6d8b8f73cbb881e93008d527441fb5161d497
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-23 00:03:08 +0800

    攻擊回合主流成
    細節部分還是以 throw new NotImplementedException(); 處理

--- DotaGame2/AttackEvent.cs
+++ DotaGame2/AttackEvent.cs
@@ -13,6 +13,78 @@ namespace DotaGame2
 
         internal bool Attack()
         {
+            var isOver = false;
+            if (!_attackStatus)
+            {
+                return isOver;
+            }
+
+            var soliders = new int[] { };
+            var villagers = new int[] { };
+            var enemyLife = 0;
+            for (int i = 0; i < soliders.Length; i++)
+            {
+                var result = DoAttack(soliders[i]);
+                SetEnemyLife(enemyLife, result);
+            }
+
+            if (enemyLife <= 0)
+            {
+                SetGameSuccess();
+                isOver = IsGameOver(true);
+                return isOver;
+            }
+
+            if (soliders.Length <= 0 && villagers.Length <= 0)
+            {
+                SetGameFail();
+                isOver = IsGameOver(true);
+                return isOver;
+            }
+
+            if (soliders.Length > 0)
+            {
+                EnemyAttack(soliders);
+            }
+            else
+            {
+                if (villagers.Length > 0)
+                {
+                    EnemyAttack(soliders);
+                }
+            }
+
+            isOver = false;
+            return isOver;
+        }
+
+        private void EnemyAttack(int[] soliders)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetGameFail()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsGameOver(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetGameSuccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetEnemyLife(int enemyLife, object result)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object DoAttack(int v)
+        {
             throw new NotImplementedException();
         }
     }
