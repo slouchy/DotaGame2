8c1f19c7d045b3d710d6519051fe31d3b2c8068a
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-22 23:43:32 +0800

    產生人員的流程

--- DotaGame2/Person.cs
+++ DotaGame2/Person.cs
@@ -29,6 +29,45 @@ namespace DotaGame2
 
         bool IPerson.Generate()
         {
+            var resource = LoadGenerateInfo();
+            var useResource = 0;
+            var isAttack = false;
+            if (resource < useResource)
+            {
+                ShowResourceNotEnough();
+                isAttack = IsAttack(false);
+                return isAttack;
+            }
+
+            resource = DeductResource(resource, useResource);
+            ShowGenerateSuccess();
+            isAttack = IsAttack(true);
+            return isAttack;
+
+        }
+
+        private void ShowGenerateSuccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int DeductResource(int resource, int useResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsAttack(bool isAttack)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowResourceNotEnough()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int LoadGenerateInfo()
+        {
             throw new NotImplementedException();
         }
 
