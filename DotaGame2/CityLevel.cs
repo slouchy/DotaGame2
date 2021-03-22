a7ae3fbf1bf03097b19958db2716eed5596a69c9
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-22 23:50:23 +0800

    升級的流程

--- DotaGame2/CityLevel.cs
+++ DotaGame2/CityLevel.cs
@@ -10,6 +10,44 @@ namespace DotaGame2
 
         internal bool LevelUp()
         {
+            var resource = LoadGenerateInfo();
+            var useResource = 0;
+            var isAttack = false;
+            if (resource < useResource)
+            {
+                ShowResourceNotEnough();
+                isAttack = IsAttack(false);
+                return isAttack;
+            }
+
+            resource = DeductResource(resource, useResource);
+            ShowGenerateSuccess();
+            isAttack = IsAttack(true);
+            return isAttack;
+        }
+
+        private int DeductResource(int resource, int useResource)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowGenerateSuccess()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsAttack(bool isAttack)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowResourceNotEnough()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int LoadGenerateInfo()
+        {
             throw new NotImplementedException();
         }
     }
