b689c722b112f09597bc91458511553d8c9d5d41
Po-CHEN LEE <asd06a@yahoo.com.tw>
2021-03-23 00:31:17 +0800

    使用者攻擊回合

--- DotaGame2/AttackEvent.cs
+++ DotaGame2/AttackEvent.cs
@@ -19,71 +19,41 @@ namespace DotaGame2
                 return isOver;
             }
 
-            var soliders = new int[] { };
-            var villagers = new int[] { };
-            var enemyLife = 0;
-            for (int i = 0; i < soliders.Length; i++)
-            {
-                var result = DoAttack(soliders[i]);
-                SetEnemyLife(enemyLife, result);
-            }
-
-            if (enemyLife <= 0)
-            {
-                SetGameSuccess();
-                isOver = IsGameOver(true);
-                return isOver;
-            }
-
-            if (soliders.Length <= 0 && villagers.Length <= 0)
-            {
-                SetGameFail();
-                isOver = IsGameOver(true);
-                return isOver;
-            }
-
-            if (soliders.Length > 0)
-            {
-                EnemyAttack(soliders);
-            }
-            else
-            {
-                if (villagers.Length > 0)
-                {
-                    EnemyAttack(soliders);
-                }
-            }
-
-            isOver = false;
+            isOver = UserAttackTurn();
             return isOver;
         }
 
-        private void EnemyAttack(int[] soliders)
+        private bool UserAttackTurn()
+        {
+            var isOver = false;
+            var soliders = new string[] { };
+            var enemyLife = 0;
+            for (int i = 0; i < soliders.Length; i++)
+            {
+                var result = SoliderAttack();
+                enemyLife -= SetEnemyLife(result);
+                if (enemyLife <= 0)
+                {
+                    setGameSuccess();
+                    isOver = true;
+                    break;
+                }
+            }
+
+            return isOver;
+        }
+
+        private void setGameSuccess()
         {
             throw new NotImplementedException();
         }
 
-        private void SetGameFail()
+        private int SetEnemyLife(int attack)
         {
             throw new NotImplementedException();
         }
 
-        private bool IsGameOver(bool v)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SetGameSuccess()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void SetEnemyLife(int enemyLife, object result)
-        {
-            throw new NotImplementedException();
-        }
-
-        private object DoAttack(int v)
+        private int SoliderAttack()
         {
             throw new NotImplementedException();
         }
